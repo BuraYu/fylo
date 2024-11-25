000a6d6375e1572ced629b23e9ddf1bdb845a6af
BuraYu <burak.yueksel@hotmail.com>
2024-11-25 23:06:07 +0100

    add close functionility, when link clicked in hamburger menu

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
   const menu = document.querySelector(".menu");
   const close = document.querySelector(".close-menu");
+  const navLinks = document.querySelectorAll(".menu-element");
 
   console.log(hamburger);
   console.log(menu);
+
   hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
     hamburger.style.display = "none";
@@ -16,17 +18,25 @@ document.addEventListener("DOMContentLoaded", () => {
     close.style.display = "none";
     hamburger.style.display = "";
   });
-});
 
-document.querySelector(".btn2").addEventListener("click", function () {
-  const emailInput = document.querySelector("#email");
-  const email = emailInput.value;
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const errorMessage = document.querySelector(".error-message");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("active");
+      close.style.display = "none";
+      hamburger.style.display = "";
+    });
+  });
 
-  if (regex.test(email)) {
-    errorMessage.style.display = "none";
-  } else {
-    errorMessage.style.display = "block";
-  }
+  document.querySelector(".btn2").addEventListener("click", function () {
+    const emailInput = document.querySelector("#email");
+    const email = emailInput.value;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const errorMessage = document.querySelector(".error-message");
+
+    if (regex.test(email)) {
+      errorMessage.style.display = "none";
+    } else {
+      errorMessage.style.display = "block";
+    }
+  });
 });
