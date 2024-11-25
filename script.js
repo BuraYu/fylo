b34a2d9145185c1a1e4b76dfe1335d1c93921fe9
BuraYu <burak.yueksel@hotmail.com>
2024-11-25 22:34:54 +0100

    add close button for hamburger menu

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
   const menu = document.querySelector(".menu");
+  const close = document.querySelector(".close-menu");
+
   console.log(hamburger);
   console.log(menu);
   hamburger.addEventListener("click", () => {
     menu.classList.toggle("active");
+    hamburger.style.display = "none";
+    close.style.display = "block";
+  });
+
+  close.addEventListener("click", () => {
+    menu.classList.remove("active");
+    close.style.display = "none";
+    hamburger.style.display = "";
   });
 });
